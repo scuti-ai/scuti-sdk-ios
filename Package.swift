b65fd1d50eefb6141e0550651dd1454edf93c631
Adrian R <arusin@mindtrust.com>
2023-10-02 16:29:34 +0200

    Added Swift package config file

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+
+let package = Package(
+    name: "ScutiSDKSwift",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "ScutiSDKSwift",
+            targets: ["ScutiSDKSwift"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "ScutiSDKSwift",
+//            exclude: ["instructions.md"],
+            dependencies: [],
+            resources: [
+                .process("navstepperinactive@2x.png"),
+                .process("new_items_flag@2x.png"),
+                .process("scuti_logo_white@2x.png"),
+            ]
+        ),
+    ]
+)
