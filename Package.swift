b681272da9b3732482856fa3e3dc9720bf488147
Adrian R <arusin@mindtrust.com>
2023-10-03 20:07:55 +0200

    Updated Swift package configuration

--- Package.swift
+++ Package.swift
@@ -5,27 +5,24 @@ import PackageDescription
 let package = Package(
     name: "ScutiSDKSwift",
     platforms: [
-        .macOS(.v10_14), .iOS(.v15)
+        .iOS("15.0")
     ],
     products: [
         .library(
             name: "ScutiSDKSwift",
             targets: ["ScutiSDKSwift"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ScutiSDKSwift",
+            path: "ScutiSDKSwift",
 //            exclude: ["instructions.md"],
-            dependencies: [],
             resources: [
-                .process("navstepperinactive@2x.png"),
-                .process("new_items_flag@2x.png"),
-                .process("scuti_logo_white@2x.png"),
+                .process("Resources"),
+//                .process("new_items_flag@2x.png"),
+//                .process("scuti_logo_white@2x.png"),
             ]
         ),
     ]
